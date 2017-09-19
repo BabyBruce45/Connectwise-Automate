a73fbae2813d38fb78f30b14606fa3caa79a0a85
Darren White <github@darrenwhite.org>
2017-09-18 21:11:11 -0700

    Fixed two errors in End Function comments

--- LabTech.psm1
+++ LabTech.psm1
@@ -1222,7 +1222,7 @@ Function Reset-LTService{
         }
         Else {$Error[0]}
     }#End End
-}#End Function Get-LTError
+}#End Function Reset-LTService
 
 Function Hide-LTAddRemove{
 <#
@@ -1617,7 +1617,7 @@ Function Set-LTLogging{
         Get-LTLogging          
     }    
   }#End End
-}#End Function Set-LTSLogging
+}#End Function Set-LTLogging
 
 Function Get-LTProbeErrors {
 <#
